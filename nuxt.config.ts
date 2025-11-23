9b30c965e8d5c2db310f8bb5a1656076d85908d2
Stanyslas Bres <dzetah@pm.me>
2025-11-23 18:40:18 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/hints',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: true,
+        arrowParens: true,
+        braceStyle: '1tbs',
+      },
+    },
+  },
+});
